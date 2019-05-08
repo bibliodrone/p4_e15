106461d8c3defdafcc94252d098eea658838d6e8
Gerry Walden <gerald.walden@gmail.com>
2019-05-07 22:33:43 -0400

    Adding debugbar

--- app/Workout.php
+++ app/Workout.php
@@ -6,17 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Workout extends Model
 {
-    public static function dump($books = null)
+    public static function dump($workouts = null)
     {
         # Empty array that will hold all our book data
         $data = [];
-        # Determine if we should use $books as was passed to this method
-        # or query for all books
+        
         if (is_null($workouts)) {
-            # Query for all the books
             $workouts = self::all();
         }
-        # Load the data array with the book info we want
         foreach ($workouts as $workout) {
             $data[] = $workout->exercise . ' ' . $workout->sets . '  ' . $workout->reps;
         }
